50917535f8f27e36e740309ff37fa188b51ab832
taptap-kor <tyy8282@gmail.com>
2023-07-05 00:17:53 +0900

    feat: add express module

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,4 +1,17 @@
 const { ethers } = require("ethers");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 /**
  * 로그인 시 유저가 회원가입을 했는지 DB 체크
