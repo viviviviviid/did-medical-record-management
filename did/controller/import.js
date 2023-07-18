633d7dec6e043b60cad94625e0c2a541968c8979
taptap-kor <tyy8282@gmail.com>
2023-07-18 21:18:26 +0900

    refactor: splited in es6, commonJs files in did

--- did/controller/import.js
+++
@@ -1,9 +0,0 @@
-import { EthrDID,DelegateTypes  } from "ethr-did";
-import { Resolver } from 'did-resolver'
-import { getResolver } from 'ethr-did-resolver'
-
-
-
-
-// const ethrDid = new EthrDID({ identifier: '0x...', privateKey: '...', provider, chainNameOrId });
-// here
