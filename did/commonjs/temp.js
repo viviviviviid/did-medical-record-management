55a3fc58edb8b8eac9c56588543b3f6f9efb7d8f
taptap-kor <tyy8282@gmail.com>
2023-08-16 19:37:12 +0900

    refactor: refactoring did folder

--- did/commonjs/temp.js
+++
@@ -1,5 +0,0 @@
-// const { ethers } = require('ethers');
-
-// let chainNameOrId = 'goerli';
-// const rpcUrl = "https://goerli.infura.io/v3/08f3fe16613d452fa248327e4d074d32";
-// const provider = new ethers.InfuraProvider(chainNameOrId, rpcUrl);
