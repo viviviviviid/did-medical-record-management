8696b79a7ab062a0d7985f28d5643ed361b0ae01
taptap-kor <tyy8282@gmail.com>
2023-08-21 20:54:53 +0900

    feat: comment out functions

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -9,58 +9,58 @@ const { medicalRecordRegister, createHash4DidUpdate, findAll } = require("./medi
  * 로그인 시 유저가 회원가입을 했는지 DB 체크
  * @returns DB 저장유무에 따른 boolean 반환 
  */
-const isUserRegistered = async (req, res) => {
-  try {
-    const access_token = req.body.token;
-    const userInfo = await axios.post("https://kapi.kakao.com/v2/user/me", {}, {  // 두번째는 받는 파라미터, 세번째가 보내는 파라미터
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      }
-    });
-    return await userFind(userInfo.data.kakao_account).then(result => {
-      result
-      ? (res.status(200).json({
-        dbData: result, 
-        msg: "already exist in DB"
-      })) 
-      : (res.status(400).send("not exist in DB")) // 이때 프론트는 회원가입 창으로 연결
-    })
-  } catch (error) {
-    return res.status(400).send(error);
-  }
-}
+// const isUserRegistered = async (req, res) => {
+//   try {
+//     const access_token = req.body.token;
+//     const userInfo = await axios.post("https://kapi.kakao.com/v2/user/me", {}, {  // 두번째는 받는 파라미터, 세번째가 보내는 파라미터
+//       headers: {
+//         Authorization: `Bearer ${access_token}`,
+//       }
+//     });
+//     return await userFind(userInfo.data.kakao_account).then(result => {
+//       result
+//       ? (res.status(200).json({
+//         dbData: result, 
+//         msg: "already exist in DB"
+//       })) 
+//       : (res.status(400).send("not exist in DB")) // 이때 프론트는 회원가입 창으로 연결
+//     })
+//   } catch (error) {
+//     return res.status(400).send(error);
+//   }
+// }
 
 /**
  * 유저가 회원가입을 진행했는지 DB 탐색
  */
-const userFind = async (userInfo) => {
-  const data = await Member.findOne({where: {email: `${userInfo.email}`}});
-  if(data !== null){
-    console.log("already exist");
-    return true;
-  }else{
-    console.log("not exist")
-    return false;
-  }
-}
+// const userFind = async (userInfo) => {
+//   const data = await Member.findOne({where: {email: `${userInfo.email}`}});
+//   if(data !== null){
+//     console.log("already exist");
+//     return true;
+//   }else{
+//     console.log("not exist")
+//     return false;
+//   }
+// }
 
 /**
  * 유저 정보 DB 등록
  */
-const userRegister = async (userInfo) => {
-  try{
-    db.User.create({
-      name: `${userInfo.name}`,
-      email: `${userInfo.email}`,
-      birthday: `${userInfo.birthday}`,
-      phoneNumber:  `${userInfo.phoneNumber}`,
-      isDoctor: `${userInfo.isDoctor}`,
-      wallet: `${userWallet.wallet}`,
-    });
-  }catch(error){
-    console.log("userRegister function Error: ",error);
-  }
-}
+// const userRegister = async (userInfo) => {
+//   try{
+//     db.User.create({
+//       name: `${userInfo.name}`,
+//       email: `${userInfo.email}`,
+//       birthday: `${userInfo.birthday}`,
+//       phoneNumber:  `${userInfo.phoneNumber}`,
+//       isDoctor: `${userInfo.isDoctor}`,
+//       wallet: `${userWallet.wallet}`,
+//     });
+//   }catch(error){
+//     console.log("userRegister function Error: ",error);
+//   }
+// }
 
 /**
  * 회원가입
@@ -68,7 +68,7 @@ const userRegister = async (userInfo) => {
 // const signUp = async (req, res) => {
 //   try {
 //     const {name, email, birthday, phoneNumber, isDoctor} = req.body.data;
-
+//
 //     // 회원가입이 완료되면 자동으로 로그인 완료 화면으로 넘기기
 //     // 지갑주소 만들어주고 레지스트리에 등록
 //     // es6 내용이므로 babel로 묶어 쓰거나, 동적 호출을 하던가.
@@ -79,16 +79,16 @@ const userRegister = async (userInfo) => {
 //       phoneNumber: phoneNumber,
 //       isDoctor: isDoctor,
 //     });
-
+//
 //     const userInfo = {name, email, birthday, phoneNumber, isDoctor, wallet};
-    
+//
 //     // 회원가입 정보와 지갑 관련 정보(PK 포함)를 DB에 저장
 //     userRegister(userInfo);
-
+//
 //   }catch(error){
 //     console.log("signUp function error: ", error);
 //   }
-  
+//
 // }
 
 /**
@@ -98,23 +98,23 @@ const userRegister = async (userInfo) => {
 //   try{
 //     // 이미 환자에게 vcJwt를 받은 후 검증하였으므로 문제가 없다고 판단.
 //     // 즉 추가되는 내용말고는 과정 필요없음.
-
+//
 //     const lastVcJwt = req.body.vcJwt;
 //     const did = jwt.decode(lastVcJwt).sub.did;
-
+//
 //     // 새롭게 추가된 진료내용을 db에 저장 
 //     medicalRecordRegister(req.body.medicalRecord);
-
+//
 //     // 방금 저장된 것을 포함, db에 저장된 환자의 모든 내용을 반환
 //     const dbData = findAll(did);
-
+//
 //     // 그 내용 중 medicalRecords 카테고리에 새로운 해시 하나를 추가
 //     const hash = createHash4DidUpdate(dbData);
-
+//
 //     // 방금 만든 hash를 넣어 vcPayload를 재구성하고 vcJwt를 만들어 서명하기
 //     // 새로 만들어진 vcJwt를 프론트에 보내기 위해 받아두기. did 폴더에서 가져와야하므로 babel 과정 거쳐야함
 //     const updatedVcJwt = update_DID(lastVcJwt, hash);
-
+//
 //     return res.status(200).send({dbData, updatedVcJwt});
 //   }catch(error){
 //     return res.status(400).send(error);
