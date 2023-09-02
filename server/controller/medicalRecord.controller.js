b6fab1d9154a21c89422eb151afce0b3011d69c8
taptap-kor <tyy8282@gmail.com>
2023-09-02 21:13:30 +0900

    feat: bridge api getAllMyPatientList

--- server/controller/medicalRecord.controller.js
+++ server/controller/medicalRecord.controller.js
@@ -1,4 +1,5 @@
 const db = require("../model/index.js");
+const jwt = require("jsonwebtoken");
 const crypto = require('crypto');
 
 /**
@@ -48,35 +49,51 @@ const createHash4DidUpdate = async (dbData) => {
   return hash.digest('hex');
 }
 const filter4nonDuplicate = async (targetArray) => {
-  const nonDuplicated_Set = new Set(targetArray);
-  const promises = [...nonDuplicated_Set].map(patientDID => 
-    db.User.findOne({
-      where: {did: patientDID},
-    })
-  );
-  return await Promise.all(promises);
+  try{
+    const nonDuplicated_Set = new Set(targetArray);
+    const promises = [...nonDuplicated_Set].map(patientDID => 
+      db.User.findOne({
+        where: {did: patientDID},
+      })
+    );
+    return await Promise.all(promises);
+  }catch(error){
+    console.log("filter4nonDuplicate function error :", error)
+    return error
+  }
 }
 
 const getAllMyRecords_DB = async (patientDID) => {
-  patientDID = JSON.stringify(patientDID);
-  console.log(patientDID);
-  return await db.MedicalRecords.findAll({
-    where: {patientDID: patientDID},
-    order: [['recordNumber', 'DESC']] // 내림차순(최근 -> 과거)로 정렬해서 변동성이 없도록
-  });
+  try{
+    patientDID = JSON.stringify(patientDID);
+    console.log(patientDID);
+    return await db.MedicalRecords.findAll({
+      where: {patientDID: patientDID},
+      order: [['recordNumber', 'DESC']] // 내림차순(최근 -> 과거)로 정렬해서 변동성이 없도록
+    });
+  }catch(error){
+    console.log("getAllMyRecords_DB function error :", error)
+    return error
+  }
+
 }
 
 const getAllMyPatientsList_DB = async (doctorDID) => {
-  doctorDID = JSON.stringify(doctorDID);
-  const dbData = await db.MedicalRecords.findAll({
-    where: {doctorDID: doctorDID},
-    attributes: ["patientDID"],
-    order: [['recordNumber', 'DESC']], // 내림차순(최근 -> 과거)로 정렬해서 변동성이 없도록
-  });
-  const patientDidList = dbData.map(el => {
-    return el.dataValues.patientDID
-  });
-  return filter4nonDuplicate(patientDidList);
+  try{
+    doctorDID = JSON.stringify(doctorDID);
+    const dbData = await db.MedicalRecords.findAll({
+      where: {doctorDID: doctorDID},
+      attributes: ["patientDID"],
+      order: [['recordNumber', 'DESC']], // 내림차순(최근 -> 과거)로 정렬해서 변동성이 없도록
+    });
+    const patientDidList = dbData.map(el => {
+      return el.dataValues.patientDID
+    });
+    return filter4nonDuplicate(patientDidList);
+  }catch(error){
+    console.log("getAllMyPatientsList_DB function error :", error)
+    return error
+  }
 }
 
 const getAllMyPatientsRecords_DB = async (doctorDID, patientDID) => {
@@ -93,17 +110,25 @@ const getAllMyPatientsRecords_DB = async (doctorDID, patientDID) => {
 
 const getAllMyPatientList = async (req, res) => {
   try{
+
     // 로그인 후 의사 개인 페이지에 온 것이므로 따로 검증할 필요는 없음
-    // const decodedPayload = await jwt.decode(req.body.vcJwt)
-    // const doctorDID = decodedPayload.sub.did;; 
+    // const decodedPayload = await jwt.decode(req.body.doctorJwt)
+    // console.log(decodedPayload.vc.credentialSubject.userInfo)
+    // const doctorDID = decodedPayload.sub.did;
+
+    // console.log("doctorDID를 가져옴: ", doctorDID);
+
+    // 환자들 유저 정보 리스트만 필요
+    // const dbData = await getAllMyPatientsList_DB(JSON.stringify(doctorDID));
 
     // 원래는 jwt에서 did를 추출하지만, 테스트용이므로 의사의 did 직접 제공
-    const doctorDID = {
+    const tempDoctorDID = {
       "did":"did:ethr:goerli:0xEC6138620175229050554653Bf36a1f49e767e8A",
       "address":"0xEC6138620175229050554653Bf36a1f49e767e8A"
     };
+
     // 환자들 유저 정보 리스트만 필요
-    const dbData = await getAllMyPatientsList_DB(doctorDID);
+    const dbData = await getAllMyPatientsList_DB(tempDoctorDID);
 
     // 유저정보에서 wallet, did는 필요없음 -> 제거하고 보내야 함
     res.status(200).send(dbData);
@@ -116,21 +141,20 @@ const getAllMyPatientList = async (req, res) => {
 const getAllMyPatientsRecords = async (req, res) => {
   try{
     // 로그인 후 의사 개인 페이지에 온 것이므로 따로 검증할 필요는 없음
-    // const decodedPayload = await jwt.decode(req.body.vcJwt);
-    // const doctorDID = decodedPayload.sub.did;
-    // const patientDID = req.body.patientDID
+    const decodedPayload = await jwt.decode(req.body.vcJwt);
+    const doctorDID = decodedPayload.sub.did;
+    const patientDID = req.body.patientDID
+    
+    console.log("doctorDID를 가져옴: ", doctorDID);
+    console.log("patientDID 가져옴: ", patientDID);
 
     // 프론트랑 연결될때까지, DID 직접 주입 => patientDID 뽑아오는게 제일 시급.s
-    const doctorDID = {
+    const tempDoctorDID = {
       "did":"did:ethr:goerli:0xEC6138620175229050554653Bf36a1f49e767e8A",
       "address":"0xEC6138620175229050554653Bf36a1f49e767e8A"
     };
-    const patientDID = {
-      "did":"did:ethr:goerli:0x6AaF555b16F897Bd5aC69BD1E52aa9d446992511",
-      "address":"0x6AaF555b16F897Bd5aC69BD1E52aa9d446992511"
-    };
 
-    const dbData = await getAllMyPatientsRecords_DB(doctorDID, patientDID);
+    const dbData = await getAllMyPatientsRecords_DB(tempDoctorDID, patientDID);
 
     res.status(200).send(dbData);
   }catch(error){
