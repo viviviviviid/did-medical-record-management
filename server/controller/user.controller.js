d78e17e89ef256e8b057cc683e197429973003b6
Minseok Seo <44859379+viviviviviid@users.noreply.github.com>
2023-09-14 06:21:36 +0900

    Update user.controller.js

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -215,7 +215,7 @@ const getDoctorWaitingList_DB = async (req, res) => {
 
 const test = async (req, res) => { 
   try{
-    console.log("success")
+    console.log("plz")
     res.status(200).send(serverIP)
   }catch(error){
     console.log("test fail")
