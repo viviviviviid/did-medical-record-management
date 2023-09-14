f89f7d3621bf6a0651b82bbbf3d5c6f17d3dd6c3
viviviviviid <tyy8282@gmail.com>
2023-09-14 09:45:04 +0000

    test

--- README.md
+++
@@ -1,1 +0,0 @@
-# did-medical-record-management
--- scripts/clean_before_deploy.sh
+++ scripts/clean_before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 배포 디렉터리 내의 모든 내용 삭제
-rm -rf /home/ubuntu/project/did-medical-record-management/*
+sudo rm -rf /home/ubuntu/project/did-medical-record-management/*
 
 # 다른 불필요한 파일이나 디렉터리도 여기에 추가하여 삭제할 수 있습니다.
-find /home/ubuntu/project/did-medical-record-management/ -name ".DS_Store" -type f -delete
+sudo find /home/ubuntu/project/did-medical-record-management/ -name ".DS_Store" -type f -delete
