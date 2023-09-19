473df237b364b53a020c860fcfba8a5dc725e253
Minseok Seo <44859379+viviviviviid@users.noreply.github.com>
2023-09-19 18:31:45 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,23 +1,28 @@
-const https = require('https');
 const fs = require('fs');
+const https = require('https');
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
 
 const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/api.dmrs.space/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/api.dmrs.space/fullchain.pem')
+  key: fs.readFileSync('/etc/letsencrypt/archive/api.dmrs.space/privkey1.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/archive/api.dmrs.space/fullchain1.pem')
 };
 
-app.use(
-  cors({ origin: true, credentials: true }),
-  express.json(),
-);
+app.get('/', (req, res) => {
+  res.send('Hello HTTPS!');
+});
 
-// 기존 라우트 추가
-require("./routes/index.js")(app);
+const server = https.createServer(options, app);
 
-https.createServer(options, app).listen(5001, () => {
-  console.log('HTTPS Server Running at 5001');
+server.listen(443, () => {
+  console.log('HTTPS Server Running at 443');
+});
+
+// 추가: Node.js 애플리케이션 실행 사용자에게 읽기 권한을 주는 부분
+fs.chmod('/etc/letsencrypt/archive/api.dmrs.space/privkey1.pem', '644', (err) => {
+  if (err) {
+    console.error('Failed to change file permissions:', err);
+  } else {
+    console.log('File permissions changed successfully.');
+  }
 });
