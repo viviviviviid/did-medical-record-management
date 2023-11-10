134b127822d13f4d814717a739e852078704a9e9
taptap-kor <tyy8282@gmail.com>
2023-11-11 00:26:24 +0900

    feat: db column name changed easily

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const cors = require("cors");
 const app = express();
 const port = 5001;
 
-const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/api.dmrs.space/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/api.dmrs.space/fullchain.pem')
-};
+// const httpsOptions = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/api.dmrs.space/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/api.dmrs.space/fullchain.pem')
+// };
 
 app.use(
   cors({ origin: true, credentials: true }),
@@ -19,13 +19,13 @@ app.get('/', (req, res) => {
     res.send('SERVER API, "/"');
 });
 
-// app.listen(port, () => {
-//     console.log("서버가 정상적으로 실행되었습니다.");
-//   });
+app.listen(port, () => {
+    console.log("서버가 정상적으로 실행되었습니다.");
+  });
 
 
-const server = https.createServer(httpsOptions, app).listen(port, () => {
-    console.log(`HTTPS 서버가 ${port} 포트에서 정상적으로 실행되었습니다.`);
-});
+// const server = https.createServer(httpsOptions, app).listen(port, () => {
+//     console.log(`HTTPS 서버가 ${port} 포트에서 정상적으로 실행되었습니다.`);
+// });
 
 require("./routes/index.js")(app);
