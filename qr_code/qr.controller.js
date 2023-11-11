930b6159c72386a2080114bed55f0a586007e456
taptap-kor <tyy8282@gmail.com>
2023-11-11 16:18:34 +0900

    feat: delete visitDate column in medicalRecords DB

--- qr_code/qr.controller.js
+++ qr_code/qr.controller.js
@@ -1,41 +1,37 @@
 
 const express = require("express");
+
 // 임시 API 관리를 위한 맵
 const tempAPIs = new Map();
 
 // 특정 비밀번호를 주고 받는다던지
 // 유효기간을 준다던지
 const requestLink_QR = async (req, res) => {
-  try {
-    const jwt = req.body.jwt;
-    const password = req.body.password;
+  try{
+    const jwt = req.body.jwt
     const tempPath = Math.random().toString(36).substring(2, 15);
 
-    tempAPIs.set(tempPath, { jwt, password });
+    // 임시 API 생성 및 설정
+    tempAPIs.set(tempPath, { jwt });
 
-    // POST 요청으로 변경
-    req.app.post(`/temp/${tempPath}`, express.json(), (req, res) => {
+    // 임시 API를 app에 추가
+    req.app.get(`/temp/${tempPath}`, (req, res) => {
       if (tempAPIs.has(tempPath)) {
         const data = tempAPIs.get(tempPath);
-        // 요청 본문의 password와 저장된 password 비교
-        if (req.body.password === data.password) {
-          res.json({ jwt: data.jwt });
-        } else {
-          res.status(403).send('Unauthorized access');
-        }
-        tempAPIs.delete(tempPath); // 첫 요청시 API 삭제
+        tempAPIs.delete(tempPath); // 첫 요청 후 API 삭제
+        res.json(data);
       } else {
         res.status(404).send('Not found or already used');
       }
     });
 
     return res.json({ link: `http://localhost:5003/temp/${tempPath}` });
-  } catch (error) {
-    console.log("requestLink_QR function error: ", error);
+  }catch(error){
+    console.log("requestLink_QR function error: ", error)
     return res.status(400).send(error);
   }
-};
+}
 
-module.exports = {
+module.exports={
   requestLink_QR
 }
