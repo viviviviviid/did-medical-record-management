928dd1b30120c3e60b8268c87fff749dae481472
taptap-kor <tyy8282@gmail.com>
2023-11-17 16:27:24 +0900

    feat: generate link function recycle

--- qr_code/routes/qr.route.js
+++ qr_code/routes/qr.route.js
@@ -2,7 +2,6 @@ const controller = require("../qr.controller.js");
 const express = require("express");
 const router = express.Router();
 
-router.post("/link", controller.generateTempLink);
-
+router.post("/link", controller.generateLink);
 
 module.exports = router;
