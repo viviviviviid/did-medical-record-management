98370e260aba4f0909b85f246f32aabeacfca2d4
taptap-kor <tyy8282@gmail.com>
2023-11-18 15:27:06 +0900

    delete: deleted client repos

--- client_app/ios/SwiftBridge.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SwiftBridge.swift
-//  client_app
-//
-//  Created by 홍승재 on 2023/09/08.
-//
-
-import Foundation
--- client_app/modules/bottomTabs.tsx/bottomTabsStyle.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
-    bottomTabs: {
-        height: 20,
-        backgroundColor: "#FFF",
-    }    
-
-})
