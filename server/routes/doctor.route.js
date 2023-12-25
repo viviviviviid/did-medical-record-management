c4775cdd21909053935cb2205d2be917e86b33d8
taptap-kor <tyy8282@gmail.com>
2023-12-25 23:43:57 +0900

    refactor

--- server/routes/doctor.route.js
+++ server/routes/doctor.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const controller_record = require("../controller/medicalRecord.controller.js");
 const controller_doctor = require("../controller/doctor.controller.js")
 
+// POST
 router.post("/get-patients-list", controller_record.getAllMyPatientList);
 router.post("/get-all-patient-records", controller_record.getAllMyPatientsRecords);
 router.post("/new-doctor", controller_doctor.doctorRegister)
